2b0f72f6c2264f5435b2da30075d2432697b1ccb
Anton <80984465+Antongo22@users.noreply.github.com>
2025-01-23 17:49:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WcfServiceLibrary1;
+using System.ServiceModel.Description;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8212/MyService");
+
+            using (ServiceHost host = new ServiceHost(typeof(Service1), baseAddress))
+            {
+                host.AddServiceEndpoint(typeof(IService1), new BasicHttpBinding(), "");
+
+                ServiceMetadataBehavior smb = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
+                if (smb == null)
+                {
+                    smb = new ServiceMetadataBehavior();
+                    smb.HttpGetEnabled = true;
+                    smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                    host.Description.Behaviors.Add(smb);
+                }
+
+                host.Open();
+
+                Console.WriteLine("The service is ready at {0}", baseAddress);
+                Console.WriteLine("Press <Enter> to stop the service.");
+                Console.ReadLine();
+
+                host.Close();
+            }
+        }
+    }
+}
---
+++ User/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ServiceModel;
+using WcfServiceLibrary1;
+
+namespace User
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+            EndpointAddress address = new EndpointAddress("http://localhost:8212/MyService");
+
+            ChannelFactory<IService1> factory = new ChannelFactory<IService1>(binding, address);
+            IService1 channel = factory.CreateChannel();
+
+            string response = channel.GetData(42);
+            Console.WriteLine("Response: {0}", response);
+
+            ((ICommunicationObject)channel).Close();
+
+            Console.WriteLine("Press <Enter> to exit.");
+            Console.ReadLine();
+        }
+    }
+}
